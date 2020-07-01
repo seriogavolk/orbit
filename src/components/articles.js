adb0f7e02e7194d3d31ce56f0b877180e1b2895e
Dmitry Volkov <Dima.volkov1994@gmail.com>
2020-06-30 18:26:29 -0600

    added cards

--- src/components/articles.js
+++ src/components/articles.js
@@ -8,17 +8,21 @@ const Article = () => {
         <>
         <div className='articleframe'>
             <div className='headerwrap'>
-        <h1>What can Orbit do for you?</h1>
-        <p>We provide solutions for comapnies who seek to improve their website's performance and seeking for a fresh new responsive look</p>
+            <h1>What can Orbit do for you?</h1>
+            <p>We provide solutions for comapnies who seek to improve their website's performance and seeking for a fresh new responsive look</p>
+                </div>
+            <div className='card1'>
+                <h1>card1</h1>
+                <Link to='/404/'><button className='btn'>Test</button></Link>
             </div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+            <div className='card2'>
+                <h1>card2</h1>
+                <Link to='/404/'><button className='btn'>Test</button></Link>
+            </div>
+            <div className='card3'>
+                <h1>card3</h1>
+                <Link to='/404/'><button className='btn'>Test</button></Link>
+            </div>
         </div>
         </>
     )
