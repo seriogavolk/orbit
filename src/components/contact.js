478920d04b30bbf1b0fa78e176aaab71fb27bba3
seriogavolkov <serioga.volkov@gmail.com>
2020-07-02 17:28:14 -0600

    state

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const ContactForm = () => {
+
+    const [name, setName] = useState('');
+    const [lastname, setLastName] = useState('');
+    const [number, setNumber] = useState('');
+    const [message, setMessage] = useState('');
+
 
     return (
         <div className='contactwrap'>
@@ -8,12 +14,15 @@ const ContactForm = () => {
         
             <div id='inquiry' >
                 <h2>Request a Free Quote</h2> 
+                <p>{`We would love to hear from you ${name} ${lastname}`}</p>
 
 
                 <div className='form1'>
                 <input type='text' 
                 name='firstname' 
                 id='first-name' 
+                value={name}
+                onChange={event => setName(event.target.value)}
                 autoComplete='off' 
                 required
                 />
@@ -22,7 +31,49 @@ const ContactForm = () => {
                 </label>
                 
                 </div>
+                <div className='form1'>
+                <input type='text' 
+                name='last-name' 
+                id='last-name' 
+                autoComplete='off' 
+                required
+                value={lastname}
+                onChange={event => setLastName(event.target.value)}
                 
+                />
+                <label id='last-name' htmlFor='last-name' className='label-name'>
+                    <span className='content-name'>Last Name</span>
+                </label>
+                </div>
+                <div className='form1'>
+                <input type='tel' 
+                name='phone' 
+                id='phone' 
+                autoComplete='off' 
+                required
+                pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}'
+                value={number}
+                onChange={event => setNumber(event.target.value)}
+                />
+                <label id='phone' htmlFor='phone' className='label-name'>
+                    <span className='content-name'>Phone Number</span>
+                </label>
+                </div>
+                <div className='form1'>
+                <input type='text' 
+                name='message' 
+                id='message' 
+                autoComplete='off' 
+                required
+                value={message}
+                onChange={event => setMessage(event.target.value)}
+                />
+                <label id='message' htmlFor='message' className='label-name'>
+                    <span className='content-name'>Message</span>
+                </label>
+                </div>
+                
+                <button className='btn1 eff' form='inquiry' type='submit' >Submit</button>
                 
                 
                 {/* {}
